12295c0390b03cf8863f13a919ba35e6b0592409
Kewyn Vieira <kewyn.comercial@gmail.com>
2024-08-09 17:20:06 -0500

    chore(version): update to 0.4.0

--- Assets/Plugins/ScoreMilk/Scripts/BridgeCommunication.cs
+++ Assets/Plugins/ScoreMilk/Scripts/BridgeCommunication.cs
@@ -25,7 +25,7 @@ namespace ScoreMilk{
         {
             PostBridgeMessageData data = new PostBridgeMessageData();
             data.message = "gameLoaded";
-            data.version = "0.3.0";
+            data.version = "0.4.0";
             data.platform = "Unity";
 
             Application.ExternalCall("postBridgeMessage", JsonUtility.ToJson(data));		
